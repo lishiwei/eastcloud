6196cca5aed4e66545fac209e4d56038fa916181
lizy <lizy@oriental-finance.com>
2017-06-15 20:11:26 +0800

    提交我的收藏

---
+++ eastcloud/src/main/java/com/orientalfinance/eastcloud/mvp/View/ActivityMyCollectionView.java
@@ -0,0 +1,11 @@
+package com.orientalfinance.eastcloud.mvp.View;
+
+import com.hannesdorfmann.mosby.mvp.MvpView;
+
+/**
+ * Created by lzy on 2017/6/15.
+ * email:lizy@oriental-finance.com
+ */
+
+public interface ActivityMyCollectionView extends MvpView {
+}
